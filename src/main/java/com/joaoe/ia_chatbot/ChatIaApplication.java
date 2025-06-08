40462694787473d6353afd86f9b1762f6b0e697b
João Elias <joaoelias97@outlook.com>
2025-06-08 10:02:51 -0300

    first commit

---
+++ src/main/java/com/joaoe/ia_chatbot/ChatIaApplication.java
@@ -0,0 +1,13 @@
+package com.joaoe.ia_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatIaApplication.class, args);
+	}
+
+}
